88c608ada1511e6903e83f60cf09911e35ba0a76
RoMalms10 <156@holbertonschool.com>
2017-11-15 23:05:56 +0000

    Add struct declarations

--- holberton1.h
+++ holberton1.h
@@ -8,6 +8,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+typedef struct bin {
+	char *name;
+	/*function pointer*/
+} built-in;
+
+typedef struct ali {
+	char *name;
+	/*function pointer*/
+} alias;
+
 int _strcmp(char *s1, char*s2);
 int _strlen(char *s);
 char *_strconcat(char *s1, char *s2);
