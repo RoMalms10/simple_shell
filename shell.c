126d181ccaaa072f7c884b7e19476852c8f0e52a
RoMalms10 <156@holbertonschool.com>
2017-11-15 23:41:11 +0000

    Create the shell file

---
+++ shell.c
@@ -0,0 +1,21 @@
+#include "holberton1.h"
+
+/**
+  *
+  *
+  *
+  *
+  *
+  *
+  */
+int main(int argc, char **argv, char **env)
+{
+	int loops;
+	if (argc > 1)
+		;/*do something with argv*/
+	for (loops = 1; 1; loops++)
+	{
+
+	}
+	return (0);
+}
