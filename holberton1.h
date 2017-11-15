309458367be9040643f7983760ba1b127110b6d7
jesse.hedden <229@holbertonschool.com>
2017-11-15 16:43:31 +0000

    added _getenv.c prototype to header

--- holberton1.h
+++ holberton1.h
@@ -8,4 +8,13 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+typedef struct environment {
+	char *str;
+	char *next;
+} stuff;
+
+extern char **environ;
+
+char *_getenv(const char *name);
+
 #endif
