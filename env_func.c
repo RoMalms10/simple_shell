f0af0c3edaf09b707de89f74cf928423103bff87
RoMalms10 <156@holbertonschool.com>
2017-11-20 18:33:49 +0000

    Get rid of unused function

--- env_func.c
+++ env_func.c
@@ -1,43 +1,10 @@
 #include "holberton1.h"
 
 /**
- * _getenv - Searches the environment list for variable name
- *
- * @name: A pointer to a contant string.
- *
- * Return: A pointer to a string.
- */
-char *_getenv(const char *name)
-{
-	char *address;
-	size_t len, env_list, letter, i;
-	extern char **environ;
-
-	for (len = 0; name[len] != '\0';)
-		len++;
-
-	env_list = letter = 0;
-	while (environ[env_list] != NULL)
-	{
-		if (environ[env_list][0] == name[letter])
-		{
-			address = environ[env_list];
-			i = 0;
-			while (name[letter] != '\0' && environ[env_list][i] == name[letter])
-			{
-				i++;
-				letter++;
-				if (letter == len)
-					return (address);
-				else if (environ[env_list][i] != name[letter])
-					letter = 0;
-			}
-		}
-		env_list++;
-	}
-	return (NULL);
-}
-
+  *
+  *
+  *
+  */
 char *find_path(char *name)
 {
 	int x;
